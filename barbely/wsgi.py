151d21e959c3a360e3337ad36f9884baecd1939c
kauatorresfranca <kauatorresfranca2@gmail.com>
2025-04-29 16:38:51 -0300

    Adiciona log de requisições no nível WSGI

--- barbely/wsgi.py
+++ barbely/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for barbely project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.info("Inicializando WSGI application")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "barbely.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barbely.settings')
 
 application = get_wsgi_application()
+
+# Log todas as requisições no nível do WSGI
+def log_request(environ, start_response):
+    logger.info(f"WSGI Request: {environ['REQUEST_METHOD']} {environ['PATH_INFO']}")
+    return application(environ, start_response)
+
+application = log_request
