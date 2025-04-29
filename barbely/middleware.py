dc63ee1aa30f4633a119eeb2101cd1d4fc6c44fd
kauatorresfranca <kauatorresfranca2@gmail.com>
2025-04-29 16:48:41 -0300

    Remove forçamento de headers de CORS no middleware

--- barbely/middleware.py
+++ barbely/middleware.py
@@ -11,9 +11,6 @@ class LogHeadersMiddleware:
         logger.info(f"Request Method: {request.method}, Path: {request.path}")
         logger.info(f"Request Origin: {request.headers.get('Origin')}")
         response = self.get_response(request)
-        response['Access-Control-Allow-Origin'] = 'https://barbely.vercel.app'  # Forçar o header
-        response['Access-Control-Allow-Methods'] = 'DELETE, GET, OPTIONS, PATCH, POST, PUT'
-        response['Access-Control-Allow-Headers'] = 'accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with'
         logger.info(f"Response Status: {response.status_code}")
         logger.info(f"Response Headers: {response.headers}")
         return response
